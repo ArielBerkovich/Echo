e0f4f148decfced42e59e4e3c25dc09fe35e15b5
ariel <Arielberkovich1@gmail.com>
2026-08-01 16:22:53 +0300

    Refine auth hero branding

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -297,6 +297,7 @@ export default function Login({ onAuthed, initialError = "" }) {
               </div>
               <Logo size={132} />
             </div>
+            <div className="auth-hero-name">Echo</div>
           </div>
         </aside>
 
