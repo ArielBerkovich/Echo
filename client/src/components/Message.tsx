5ae54790f2491c978ad651be119acb63b5e4017f
ariel <Arielberkovich1@gmail.com>
2026-09-19 15:55:28 +0300

    Raise message action toolbar

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -229,7 +229,7 @@ function Message({
       const isRtl = document.documentElement.dataset.interfaceDirection === "rtl";
       setActionsPosition({
         top: Math.min(
-          Math.max(rect.top + 2, topBoundary),
+          Math.max(rect.top - 18, topBoundary),
           Math.max(topBoundary, window.innerHeight - toolbarHeight - 8)
         ),
         ...(isRtl
